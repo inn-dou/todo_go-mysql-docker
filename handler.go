cd8f2caf6098693cdba3dcb9358440ac83f2b462
inn-dou <hayato182010519@gmail.com>
2024-04-28 21:12:45 +0900

    Second commit

---
+++ handler.go
@@ -0,0 +1,20 @@
+package main
+
+// func getTodoList(c *gin.Context) {
+// 	// Todoの取得と表示
+// 	var todos []Todo
+// 	db.Find(&todos)
+// 	c.HTML(http.StatusOK, "index.html", todos)
+// }
+
+// func getTodoByID(c *gin.Context) {
+// 	// IDによるTodoの取得と表示
+// }
+
+// func updateTodo(c *gin.Context) {
+// 	// Todoの更新
+// }
+
+// func deleteTodo(c *gin.Context) {
+// 	// Todoの削除
+// }
